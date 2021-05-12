7ba2a361b901219ad5cdb0b9eaaf2999993cf2d5
altantur <turuu.br@gmail.com>
2021-05-13 01:14:40 +0800

    Added beautify option

--- src/App.js
+++ src/App.js
@@ -11,10 +11,6 @@ class App extends React.Component {
       mode: "rtc",
       codec: "vp8",
     })
-    const hosts = {
-      guest1: null,
-      guest2: null,
-    }
     const qry = qs.parse(window.location.search)
     const localStream = AgoraRTC.createStream({
       audio: true,
@@ -23,67 +19,83 @@ class App extends React.Component {
     this.state = { 
       muted: false, 
       noVideo: false,
+      beautified: false,
       client,
-      hosts,
       localStream,
       qry
     }
     client.init(process.env.REACT_APP_APP_ID, () => {
-      console.log("client initialized")
+      client.join(process.env.REACT_APP_API_KEY, process.env.REACT_APP_CHANNEL, qry.host || null, (uid)=>{
+        if ('host' in qry) {
+          localStream.init(()=>{
+            this.setState(state => ({
+              localStream
+            }))
+            localStream.play("me")
+            client.publish(localStream, (err) => {})
+          }, (err) => {})
+        }
+      }, (err) => {})
+      client.on("stream-added", function(evt){
+        client.subscribe(evt.stream, (err) => {});
+      });
+      client.on("stream-subscribed", function(evt){
+        console.log("----------------------------------------------------")
+        console.log(evt)
+        console.log("----------------------------------------------------")
+        const stream = evt.stream;
+        const guest1 = document.getElementById('guest1');
+        const guest2 = document.getElementById('guest2');
+        if (guest1 && !guest1.innerHTML) {
+          stream.play('guest1')
+        }
+        if (!guest2.innerHTML) {
+          stream.play('guest2')
+        }
+      });
+      client.on("stream-removed", function(evt){
+      });
+      client.on("peer-leave", function(evt){
+      });
     }, (err) => {
       console.log("client init failed ", err)
     })
-    client.join(process.env.REACT_APP_API_KEY, process.env.REACT_APP_CHANNEL, qry.host || null, (uid)=>{
-      if ('host' in qry) {
-        localStream.init(()=>{
-          this.setState(state => ({
-            localStream
-          }))
-          localStream.play("me")
-          client.publish(localStream, (err) => {})
-        }, (err) => {})
-      }
-    }, (err) => {})
-    client.on("stream-added", function(evt){
-      client.subscribe(evt.stream, (err) => {});
-    });
-    client.on("stream-subscribed", function(evt){
-      console.log(evt)
-      const stream = evt.stream;
-      const guest1 = document.getElementById('guest1');
-      const guest2 = document.getElementById('guest2');
-      if (guest1 && !guest1.innerHTML) {
-        stream.play('guest1')
-      }
-      if (!guest2.innerHTML) {
-        stream.play('guest2')
-      }
-    });
-    client.on("stream-removed", function(evt){
-    });
-    client.on("peer-leave", function(evt){
-    });
+    
     this.toggleAudio = this.toggleAudio.bind(this)
     this.toggleVideo = this.toggleVideo.bind(this)
+    this.toggleBeautify = this.toggleBeautify.bind(this)
+  }
+
+  async toggleBeautify () {
+    if (this.state.beautified) {
+      await this.state.localStream.setBeautyEffectOptions(true, {
+        lighteningContrastLevel: 2,
+        lighteningLevel: 1.0,
+        smoothnessLevel: 1.0,
+        rednessLevel: 0.1
+      })
+    } else {
+      await this.state.localStream.setBeautyEffectOptions(true, {
+        lighteningContrastLevel: 1,
+        lighteningLevel: 0.7,
+        smoothnessLevel: 0.5,
+        rednessLevel: 0.1
+      })
+    }
+    this.setState(state => ({
+      beautified: !this.state.beautified
+    }));
   }
 
   toggleAudio () {
-    if (this.state.muted) {
-      this.state.localStream.unmuteAudio()
-    } else {
-      this.state.localStream.muteAudio()
-    }
+    this.state.muted ?  this.state.localStream.unmuteAudio() : this.state.localStream.muteAudio()
     this.setState(state => ({
       muted: !this.state.muted
     }));
   }
 
   toggleVideo () {
-    if (this.state.noVideo) {
-      this.state.localStream.unmuteVideo()
-    } else {
-      this.state.localStream.muteVideo()
-    }
+    this.state.noVideo ?  this.state.localStream.unmuteVideo() : this.state.localStream.muteVideo()
     this.setState(state => ({
       noVideo: !state.noVideo
     }));
@@ -101,6 +113,9 @@ class App extends React.Component {
               </div>
               <div className={ this.state.noVideo ? 'centered clicked' : 'centered'} onClick={this.toggleVideo}>
                 { this.state.noVideo ? '😵 Camera Disabled' : '😳 Disable Camera'}
+              </div>
+              <div className={ this.state.beautified ? 'centered clicked' : 'centered'} onClick={this.toggleBeautify}>
+                { this.state.beautified ? '💄 Beautified' : '🤔 Beautify'}
               </div>
             </div>
             <div className="centered view-count">
